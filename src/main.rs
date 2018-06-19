500c06956ec40677d5967f17bb9d8ce8122173bd
wraikny <writtenknown@gmail.com>
2018-06-19 18:39:34 +0900

    Error enum

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,54 @@
 extern crate raytrace;
 use raytrace::{io, render};
 
-fn main() -> Result<(), Box<std::error::Error>> {
+use std::fmt;
+extern crate rayon;
+
+enum MyError {
+    Run(rayon::ThreadPoolBuildError),
+    WriteImage(io::WriteImageError),
+    Command(std::io::Error),
+}
+
+impl std::convert::From<rayon::ThreadPoolBuildError> for MyError {
+    fn from(error : rayon::ThreadPoolBuildError) -> MyError {
+        MyError::Run(error)
+    }
+}
+
+impl std::convert::From<io::WriteImageError> for MyError {
+    fn from(error : io::WriteImageError) -> MyError {
+        MyError::WriteImage(error)
+    }
+}
+
+impl std::convert::From<std::io::Error> for MyError {
+    fn from(error : std::io::Error) -> MyError {
+        MyError::Command(error)
+    }
+}
+
+impl std::fmt::Debug for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use MyError::*;
+        
+        match self {
+            Run(e) => write!(f, "{:?}", e),
+            WriteImage(e) => write!(f, "{:?}", e),
+            Command(e) => write!(f, "{:?}", e),
+        }
+        
+    }
+}
+
+fn main() -> Result<(), MyError> {
     let rs = render::RenderSetting {
         spp : 500,
         reflect_n : 20,
         .. Default::default()
     };
 
-    let cs = render::run(&rs, 500)?;
+    let cs = render::run(&rs, 9)?;
     let f = format!("result-{}-{}", &rs.spp, &rs.reflect_n);
     io::write_image(rs.window_size, cs, f.clone())?;
 
