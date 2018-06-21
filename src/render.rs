447f7671d0bdd8ae3ad0bfbf94cba295f591583c
wraikny <writtenknown@gmail.com>
2018-06-21 13:57:22 +0900

    fix mistake (forgotten sqrt)

--- src/render.rs
+++ src/render.rs
@@ -76,102 +76,111 @@ pub fn run(rs : &RenderSetting) -> Result<Vec<(u8, u8, u8)>, rayon::ThreadPoolBu
             let create_ray = |rx, ry| c.create_ray((w, h), (rx, ry));
             
             let v : Vec3 = match rs.mode {
-                RenderMode::Shade => {
-                    (0..rs.spp).into_par_iter().map(|_|{
-                        let mut ray = create_ray(x + random::<f64>(), y + random::<f64>());
+                RenderMode::Shade => (0..rs.spp).into_par_iter().map(|_|{
+                    let mut ray = create_ray(x + random::<f64>(), y + random::<f64>());
 
-                        let mut sum = Vec3::new(0.0);
-                        let mut thp = Vec3::new(1.0);
+                    let mut sum = Vec3::new(0.0);
+                    let mut thp = Vec3::new(1.0);
 
-                        'reflect: for _depth in 0..rs.reflect_n {
-                            let h = rs.scene.hit(&ray, (0.1f64.powi(4), 10.0f64.powi(10)));
+                    'reflect: for _depth in 0..rs.reflect_n {
+                        let h = rs.scene.hit(&ray, (0.1f64.powi(4), 10.0f64.powi(10)));
 
-                            if let Some(hr) = h {
-                                sum = sum + thp * hr.le;
-                                ray = Ray {
-                                    origin : hr.point,
-                                    direction : match hr.material {
-                                        Material::Diffuse => {
-                                            let n = if hr.normal.dot(&-ray.direction) > 0.0 {
-                                                hr.normal
-                                            } else {
-                                                -hr.normal
-                                            };
-                                            let (u, v) = {
-                                                let t = TangentSpace::new(&n);
-                                                (t.0, t.1)
-                                            };
-                                            let d = {
-                                                let r = random::<f64>().sqrt();
-                                                let t : f64 = 2.0 * std::f64::consts::PI * random::<f64>();
-                                                let (x, y) = (r * t.cos(), r * t.sin());
-                                                Vec3{x, y, z : 0.0f64.max(1.0 - (x * x + y * y))}
-                                            };
-                                            u * d.x + v * d.y + n * d.z
-                                        },
-                                        Material::Mirror => {
-                                                let wi = -ray.direction;
-                                                hr.normal * 2.0 * wi.dot(&hr.normal) - wi
-                                        },
-                                        Material::Fresnel(ior) => {
-                                            let wi = -ray.direction;
-                                            let into = wi.dot(&hr.normal) > 0.0;
-                                            let n = hr.normal * if into {1.0} else {-1.0};
-                                            let eta = if into {
-                                                1.0 / ior
-                                            } else {
-                                                ior
-                                            };
+                        if let Some(hr) = h {
+                            sum = sum + thp * hr.le;
 
-                                            let wt = {
-                                                // Snell's law (vector form)
-                                                let t = wi.dot(&n);
-                                                let t2 = 1.0 - eta * eta * (1.0 - t * t);
-                                                if t2 < 0.0 {
-                                                    None
-                                                } else {
-                                                    Some((n * t - wi) * eta - n * t2.sqrt())
-                                                }
-                                            };
+                            let reflect_mirror = move || {
+                                let wi = -ray.direction;
+                                hr.normal * 2.0 * wi.dot(&hr.normal) - wi
+                            };
 
-                                            if let Some(wt) = wt {
-                                                // Schlick's approximation
-                                                let fr = {
-                                                    let cos = if into {
-                                                        wi.dot(&hr.normal)
-                                                    } else {
-                                                        wt.dot(&hr.normal)
-                                                    };
-                                                    let r = (1.0 - ior) / (1.0 + ior);
-                                                    r * r + (1.0 - r * r) * (1.0 - cos).powi(5)
-                                                };
-                                                // Select reflection or refraction
-                                                // according to the fresnel term
+                            // Update Ray
+                            ray = Ray {
+                                origin : hr.point,
+                                direction : match hr.material {
+                                    Material::Diffuse => {
+                                        let n = hr.normal * if hr.normal.dot(&-ray.direction) > 0.0 {
+                                            1.0
+                                        } else {
+                                            -1.0
+                                        };
 
-                                                if random::<f64>() < fr {
-                                                    hr.normal * 2.0 * wi.dot(&hr.normal) - wi
-                                                } else {
-                                                    wt
-                                                }
-                                            } else {
-                                                // Total internal reflection
-                                                hr.normal * 2.0 * wi.dot(&hr.normal) - wi
+                                        let (u, v) = {
+                                            let t = TangentSpace::new(&n);
+                                            (t.0, t.1)
+                                        };
+                                        
+                                        let d = {
+                                            let r = random::<f64>().sqrt();
+                                            let t : f64 = 2.0 * std::f64::consts::PI * random::<f64>();
+                                            let (x, y) = (r * t.cos(), r * t.sin());
+                                            Vec3{x, y,
+                                                z : 0.0f64.max(1.0 - x.powi(2) - y.powi(2)).sqrt()
                                             }
+                                        };
+                                        u * d.x + v * d.y + n * d.z
+                                    },
+                                    Material::Mirror => {
+                                            reflect_mirror()
+                                    },
+                                    Material::Fresnel(ior) => {
+                                        let wi = -ray.direction;
+                                        let into = wi.dot(&hr.normal) > 0.0;
+                                        let n = hr.normal * if into {1.0} else {-1.0};
+                                        let eta = if into {
+                                            1.0 / ior
+                                        } else {
+                                            ior
+                                        };
+
+                                        let wt = {
+                                            // Snell's law (vector form)
+                                            let t = wi.dot(&n);
+                                            let t2 = 1.0 - eta.powi(2) * (1.0 - t.powi(2));
+                                            if t2 < 0.0 {
+                                                None
+                                            } else {
+                                                Some((n * t - wi) * eta - n * t2.sqrt())
+                                            }
+                                        };
+
+                                        if let Some(wt) = wt {
+                                            // Schlick's approximation
+                                            let fr = {
+                                                let cos = if into {
+                                                    wi.dot(&hr.normal)
+                                                } else {
+                                                    wt.dot(&hr.normal)
+                                                };
+                                                let r = (1.0 - ior) / (1.0 + ior);
+                                                r * r + (1.0 - r.powi(2)) * (1.0 - cos).powi(5)
+                                            };
+                                            // Select reflection or refraction
+                                            // according to the fresnel term
+
+                                            if random::<f64>() < fr {
+                                                reflect_mirror()
+                                            } else {
+                                                wt
+                                            }
+                                        } else {
+                                            // Total internal reflection
+                                            reflect_mirror()
                                         }
                                     }
-                                };
-                                
-                                // Update throughput
-                                thp = thp * hr.reflectance;
-                            }
+                                }
+                            };
                             
-                            if thp.x.max(thp.y.max(thp.z)) == 0.0 {
-                                break 'reflect;
-                            }
+                            // Update throughput
+                            thp = thp * hr.reflectance;
                         }
-                        sum / (rs.spp as f64)
-                    }).reduce(|| Vec3::new(0.0), |s, x| s + x)
-                },
+                        
+                        if thp.x.max(thp.y.max(thp.z)) == 0.0 {
+                            break 'reflect;
+                        }
+                    }
+                    sum / (rs.spp as f64)
+                }).reduce(|| Vec3::new(0.0), |s, x| s + x),
+
                 RenderMode::Normal => {
                     let ray = create_ray(x, y);
 
